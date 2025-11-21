9b1bd22510dede7ae9af61c71d8512b9fa115e00
andersonestrada-rgb <anderson.estrada@tecsup.edu.pe>
2025-11-21 12:35:51 -0500

    Update ControlEnemigo.cs

--- Assets/Scripts/Enemigos/ControlEnemigo.cs
+++ Assets/Scripts/Enemigos/ControlEnemigo.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
-    public Transform Doro;   // Referencia al Player
-    public float speed = 3f;   // Velocidad del enemigo
+    public Transform Doro;   
+    public float speed = 3f;   
 
     void Update()
     {
-        // 1. Calcular dirección hacia el jugador (Vector2)
+        
         Vector2 direction = (Vector2)Doro.position - (Vector2)transform.position;
 
-        // 2. Normalizar la dirección
+        
         direction = direction.normalized;
 
-        // 3. Mover al enemigo usando solo Vector2
+        
         transform.position = (Vector2)transform.position + direction * speed * Time.deltaTime;
     }
 }
