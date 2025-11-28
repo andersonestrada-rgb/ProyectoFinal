7da215478ac724dadd545aea09ccc2f5fe4ae3c7
andersonestrada-rgb <anderson.estrada@tecsup.edu.pe>
2025-11-28 13:04:25 -0500

    V5.15
    Cambios:
    Script de cámara

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,14 +1,43 @@
 using UnityEngine;
 
-public class C_SeguirCamara : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public Transform Player; //Ubicación del player en la escena
-    public float suavizadoDeMovimiento = 5f; 
+    public Transform Player; //Ubicación del player en la escena    
+    public float suavizadoDeMovimiento = 5f;
+
+    public GameObject Target;
+    public float cameraSpeed;
+    public float cameraRange;
+
+    private void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player");
+    }
 
     void Update()
     {
+        CameraMovementMachine();
+        /*
         if (Player == null) return;
         Vector2 nuevaPosicion = new Vector2(Player.position.x, Player.position.y);
         transform.position = Vector2.Lerp(transform.position, nuevaPosicion, suavizadoDeMovimiento * Time.deltaTime);
+        */
     }
+
+
+    public void CameraMovementMachine()
+    {
+        float distance = Vector2.Distance(Target.transform.position, transform.position);
+
+        if (distance >= cameraRange)
+        {
+            Vector3 target = Target.transform.position;
+            target.z = -10;
+            Vector3 dir = (Target.transform.position - transform.position).normalized;
+            transform.position += dir * cameraSpeed * Time.deltaTime;
+        }
+    }
+
+
+
 }
